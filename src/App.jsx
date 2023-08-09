a0b88bbb29112701462ea68fb0ed12c02edfa772
Vigneshs19 <svignesh19042000@gmail.com>
2023-08-09 19:50:40 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import {Price1} from './Price1.jsx'
+import './Price.css'
+
+function App() {
+  return (
+     <div>
+      <Price1></Price1>
+    </div>
+  )
+}
+
+export default App
